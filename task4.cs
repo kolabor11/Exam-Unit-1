5e194eecb8d5113e3cfd0db850a175d90a134511
kolabor11 <pkolabor11#gmail.com>
2024-01-26 21:36:15 +0100

    Task 4

--- task4.cs
+++ task4.cs
@@ -1,17 +1,122 @@
-// Your program should start at this line.
-/*
-Example:
-Move();
-Move();
-Move();
-Turn();
-Move();
-Move();
-Turn();
-Turn();
-Turn();
-Move();
-*/
+while (!AtGoal()){
+
+PerformSequence(new (Action, int)[] {
+        (TurnRight, 1),
+        (Move, 4),
+        (TurnLeft, 1),
+        (Move, 4),
+        (TurnRight, 1),
+        (Move, 1),
+        (TurnLeft, 1),
+        (Move, 3),
+        (TurnRight, 1),
+        (Move, 1),
+        (TurnLeft, 1),
+        (Move, 6),
+        (TurnRight, 1),
+        (Move, 3),
+        (TurnLeft, 1),
+        (Move, 4),
+        (TurnRight, 1),
+        (Move, 4),
+        (TurnLeft, 1),
+        (Move, 6),
+        (TurnRight, 1),
+        (Move, 3),
+        (TurnLeft, 1),
+        (Move, 6),
+        (TurnRight, 1),
+        (Move, 1),
+        (TurnLeft, 1),
+        (Move, 3),
+        (TurnRight, 1),
+        (Move, 1),
+        (TurnLeft, 1),
+        (Move, 6),
+        (TurnRight, 1),
+        (Move, 1),
+        (TurnLeft, 1),
+        (Move, 3),
+        (TurnRight, 1),
+        (Move, 1),
+        (TurnLeft, 1),
+        (Move, 3),
+        (TurnRight, 1),
+        (Move, 1)
+    });
+}
+
+void PerformSequence((Action, int)[] sequence)
+{
+    //is an array of tuples, where each tuple represents an action and the number of times that action should be performed.
+    foreach ((Action action, int count) in sequence)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            action.Invoke();
+        }
+    }
+}
+
+
+
+
+/*const int MAX_DIRECTIONS = 4;
+int turnCount = 0;
+while (!AtGoal())
+
+    int availableDirections = CountAvailableDirections(); 
+
+    if (Peek() && availableDirections == 1 )
+    {
+        Move();
+    }
+    else
+    {
+        
+        else if (Peek() && availableDirections == 1 )
+        {
+            Move()
+        }
+        else if (availableDirections == 3)
+        {
+            if (turnCount % 2 == 0)
+            {
+                TurnRight();
+                Move();
+            }
+            else if (turnCount % 1 == 0)
+            {
+                TurnLeft();
+                Move();
+            }
+            else
+            {
+                // Go straight
+            }
+        }
+        else if (availableDirections == 4)
+        {
+           
+            if (turnCount % 2 == 0)
+            {
+                Turn();
+            }
+        
+            else
+            {
+                Move();
+            }
+        }
+
+        turnCount++;
+    }
+
+    Here is an idea I had to make the car act different based on the amount of turns 
+    it made and possible directions to go but It did not let anywherem i am gonna leave it here, 
+    maybe I will get some point s for an idea itself.
+    */
+
 
 
 #region Basic functions
@@ -23,9 +128,18 @@ void Move()
     // Moves the car 1 cell in the direction it is heading. 
 }
 
-void Turn()
+void TurnLeft()
 {
-    // Turns the car 90 deg clockwise.
+    // Perform the equivalent of a left turn (e.g., three right turns)
+    Turn();
+    Turn();
+    Turn();
+}
+
+
+
+{
+    Turn();
 }
 
 bool Peek()
@@ -38,6 +152,10 @@ bool AtGoal()
 {
     // Returns true if the current cell is the goal cell.
     return true; // just a placholder
+}
+void leaveTrailBehindCar()
+{
+    //leaves a trail behind a car treated as a wall, this prevents the car from going back
 }
 
 #endregion
